1013648a61da0582d8e3e97e2d899152805c1b47
Armando <armandt32@gmail.com>
2020-12-10 00:04:27 -0600

    Actualización en busqueda primero anchura y primero profundidad

--- grafos/busqueda_semantica_anchura.py
+++ grafos/busqueda_semantica_anchura.py
@@ -10,29 +10,37 @@ print("Muestra el grafo antes del recorrido: \n")
 for key, lista in grafo.items():
     print(key, lista)
 
-        
-visitados = []
-pila = []
-origen = '10'
-print("\nLista de recorrido en profundidad\n")
-#Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA PILA
-pila.append(origen)
-#Paso 2: MIENTRAS LA PILA NO ESTE VACÍA
-while pila:
-    #paso 3: DESENCOLAR UN VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
-    actual = pila.pop(0)
+print("\nLista de recorrido en anchura\n")
 
-    #paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
-    if actual not in visitados:
-        #paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
+def anchura():      
+    visitados = []
+    pila = []
+    origen = '10'
+    destino = '14'
+    #Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA PILA
+    pila.append(origen)
+    #Paso 2: MIENTRAS LA PILA NO ESTE VACÍA
+    while pila:
+        #paso 3: DESENCOLAR UN VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
+        actual = pila.pop(0)###IMPORTANTE, EL 0 EN ESTA LINE HACE QUE SEA O BUSQUEDA EN PROFUNDIDAD O EN ANCHURA
         print("Vertice actual -> ", actual)
-        #paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
-        visitados.append(actual)
-    #paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO DESTINO,
-    #        Y QUE NO HA SIDO VISITADO:
-    #        ENCOLAR EL VERTICE
-    for key in grafo[actual]:
-        if key not in visitados:
-            pila.append(key)
+        if actual==destino:
+            visitados.append(actual)
+            print(visitados)
+            return
+        #paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
+        if actual not in visitados:
+            #paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
+            
+            #paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
+            visitados.append(actual)
+        #paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO DESTINO,
+        #        Y QUE NO HA SIDO VISITADO:
+        #        ENCOLAR EL VERTICE
+        for key in grafo[actual]:
+            if key not in visitados:
+                pila.append(key)
 
-print(visitados)
+    print(visitados)
+
+anchura()
--- grafos/busqueda_semantica_profundidad.py
+++ grafos/busqueda_semantica_profundidad.py
@@ -10,32 +10,40 @@ print("Muestra el grafo antes del recorrido: \n")
 for key, lista in grafo.items():
     print(key, lista)
 
-        
-visitados = []
-pila = []
-origen = '10'
-print("\nLista de recorrido en profundidad\n")
-#Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA PILA
-pila.append(origen)
-#Paso 2: MIENTRAS LA PILA NO ESTE VACÍA
-while pila:
-    #paso 3: DESAPILAR UN VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
-    actual = pila.pop()
-    #FORMA ALTERNATIVA PARA DESAPILAR:
-    #actual = pila[-1]
-    #pila.remove(pila[-1])
-
-    #paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
-    if actual not in visitados:
-        #paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
+def profundidad():      
+    visitados = []
+    pila = []
+    origen = '10'
+    destino = '14'
+    print("\nLista de recorrido en profundidad\n")
+    #Paso 1: SE COLOCA EL VÉRTICE ORIGEN EN UNA PILA
+    pila.append(origen)
+    #Paso 2: MIENTRAS LA PILA NO ESTE VACÍA
+    while pila:
+        #paso 3: DESAPILAR UN VÉRTICE, ESTE SERÁ AHORA EL VÉRTICE ACTUAL
+        actual = pila.pop()
         print("Vertice actual -> ", actual)
-        #paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
-        visitados.append(actual)
-    #paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO DESTINO,
-    #        Y QUE NO HA SIDO VISITADO:
-    #        APILAR EL VERTICE
-    for key in grafo[actual]:
-        if key not in visitados:
-            pila.append(key)
+        #FORMA ALTERNATIVA PARA DESAPILAR:
+        #actual = pila[-1]
+        #pila.remove(pila[-1])
+        if actual==destino:
+            visitados.append(actual)
+            print(visitados)
+            return
 
-print(visitados)
+        #paso 4: SI EL VÉRTICE ACTUAL NO HA SIDO VISITADO
+        if actual not in visitados:
+            #paso 5: PROCESAR (IMPRIMIR) EL VÉRTICE ACTUAL
+            
+            #paso 6: COLOCAR VÉRTICE ACTUAL EN LA LISTA DE VISITADOS
+            visitados.append(actual)
+        #paso 7: PARA CADA VÉRTICE QUE EL VÉRTICE ACTUAL TIENE COMO DESTINO,
+        #        Y QUE NO HA SIDO VISITADO:
+        #        APILAR EL VERTICE
+        for key in grafo[actual]:
+            if key not in visitados:
+                pila.append(key)
+
+    print(visitados)
+
+profundidad()
